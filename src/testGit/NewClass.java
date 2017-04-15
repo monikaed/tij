b574fac4f348a8d83f9fcc6e023071caede31a0f
Monika Ewa <danielew38@gmail.com>
2017-04-15 12:56:52 +0200

    Dodanie NewClass

---
+++ src/testGit/NewClass.java
@@ -0,0 +1,15 @@
+package testGit;
+
+public class NewClass extends Test1 {
+
+	NewClass() {
+		super("New Class");
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
